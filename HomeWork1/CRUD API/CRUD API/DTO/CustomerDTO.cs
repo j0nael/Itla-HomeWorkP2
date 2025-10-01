9de3136a18479cffb313ca8cdf5b558b07df33cd
Jonathan Geraldo <Thucientifico10@gmail.com>
2025-10-01 13:52:50 -0400

    creating history of solution

---
+++ HomeWork1/CRUD API/CRUD API/DTO/CustomerDTO.cs
@@ -0,0 +1,14 @@
+﻿using CRUD_API.Model.Entyties;
+
+namespace CRUD_API.DTO
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}

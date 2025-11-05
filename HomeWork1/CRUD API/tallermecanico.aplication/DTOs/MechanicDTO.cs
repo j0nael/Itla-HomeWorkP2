8befb6734d155aa96acc28dd07da68568af242dd
Jonathan Geraldo <Thucientifico10@gmail.com>
2025-11-05 16:15:06 -0400

    delete
    delete

--- HomeWork1/CRUD API/tallermecanico.aplication/DTOs/MechanicDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace tallermecanico.aplication.DTOs
-{
-    public class MechanicDTO
-    {
-        public int MechanicId { get; set; }
-        public string FirstName { get; set; }
-        public string Specialty { get; set; }
-    }
-}

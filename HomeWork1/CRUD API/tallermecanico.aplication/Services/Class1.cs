010e2a2cc1fac81a4e4574832340de0fb1454d6f
Jonathan Geraldo <Thucientifico10@gmail.com>
2025-11-10 22:31:17 -0400

    kj

--- HomeWork1/CRUD API/tallermecanico.aplication/Services/Class1.cs
+++ HomeWork1/CRUD API/tallermecanico.aplication/Services/Class1.cs
@@ -1,8 +1,9 @@
-﻿namespace tallermecanico.aplication.Services
+﻿using tallermecanico.infretruture.Interfaces;
+namespace tallermecanico.aplication.Services
 {
     public class CustomerService
     {
-        //private readonly ICustomer _customerService;
+        private readonly ICustomer _customerService;
         public CustomerService() { }
     }
 }

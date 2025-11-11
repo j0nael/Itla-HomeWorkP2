5e4ab4b227d3134bd9f7ebda7e760ba8cefc620b
Jonathan Geraldo <Thucientifico10@gmail.com>
2025-11-10 22:52:44 -0400

    delete
    delete

---
+++ HomeWork1/CRUD API/CRUD API/DTOs/RepairDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tallermecanico.aplication.DTOs
+{
+    public class RepairDTO
+    {
+        public int RepairId { get; set; }
+        public int VehicleId { get; set; }
+        public string LicensePlate { get; set; }
+        public int MechanicId { get; set; }
+        public int CustomerId { get; set; }
+        public int? InvoiceId { get; set; }
+        public string Description { get; set; }
+        public double Cost { get; set; }
+        public DateTime Date { get; set; }
+    }
+}

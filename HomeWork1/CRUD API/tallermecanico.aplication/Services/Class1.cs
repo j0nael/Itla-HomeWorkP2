626157b51f997c8083d2b257dc877a8a95a9dbd2
Jonathan Geraldo <Thucientifico10@gmail.com>
2025-11-10 22:48:59 -0400

    detete
    delete

--- HomeWork1/CRUD API/tallermecanico.aplication/Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace tallermecanico.aplication.Services
-{
-    public class CustomerService
-    {
-        //private readonly ICustomer _customerService;
-        public CustomerService() { }
-    }
-}
